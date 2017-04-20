2530883e82296b07a62016ac5fdbc11fd6ef0d9e
Cyberwren <Cyberwren@hotmail.com>
2017-04-20 16:25:21 -0400

    SG Sample Exercise
    SG Sample Exercise update

--- sampleExercise.js
+++ sampleExercise.js
@@ -1,62 +1,57 @@
-//Sample Exercise
-//The objective of this exercise is to allow a user to enter a starting number an ending
-//number and a step value into three textboxes on a page. A button will be present that 
-//when clicked should output all the even numbers between the start and end value.
+// Sample Exercise
+// The objective of this exercise is to allow a user to enter a starting number an ending
+// number and a step value into three textboxes on a page. A button will be present that 
+// when clicked should output all the even numbers between the start and end value.
 
-//Error messages should be displayed if:
-//• Any entry is not numeric 
-//• The step is not a positive number 
-//• The ending number is less than or equal to the starting number
+// Error messages should be displayed if:
+// • Any entry is not numeric 
+// • The step is not a positive number 
+// • The ending number is less than or equal to the starting number
 
-//Additional Requirements:
-//• Utilize the bootstrap CSS library form inputs. • Bootstrap should be linked to the page via CDN.
+// Additional Requirements:
+// • Utilize the bootstrap CSS library form inputs. 
+// • Bootstrap should be linked to the page via CDN.
 
-//take the startNum and run it in a 3 step counter that ends with endNum
-//take these numbers and run it though a forLoop checking for even numbers via the % modulo thing
-//display the result of the % modulo in a top-down list
+// take the startNum and run it in a 3 step counter that ends with endNum
+// take these numbers and run it though a forLoop checking for even numbers via the % modulo thing
+// display the result of the % modulo in a top-down list
 
 //============================================================================
 
-
+var numbers = [];
 
 function generateNumbers() {
+	var startNum = parseInt(document.getElementById('startNum').value);
+	var endNum = parseInt(document.getElementById('endNum').value);
+	var stepNum = parseInt(document.getElementById('stepNum').value);
+	document.getElementById("printText").innerHTML = "Here are the even numbers between " + startNum + " and " + endNum + " by " + stepNum + "'s:"
 		if (stepNum < 0){
-			console.log("Must be a positive number!");
+			document.getElementById("printText").innerHTML = "Must be a positive number!";
 		}
 		else if (startNum > endNum){
-			console.log("Starting number must be smaller than the ending number!");
+			document.getElementById("printText").innerHTML = "Starting number must be smaller than the ending number!";
 		}
 		else{
 			if (startNum % 2 != 0 && stepNum % 2 === 0) {
-				console.log("This will only return odd numbers");
+				document.getElementById("printText").innerHTML = "This program cannot display odd numbers";
 		}
 			else{
 				for(var x = startNum; x <= endNum; x = x + stepNum){
 					if (x % 2 === 0){
-					document.getElementById("numList").innerHTML = x;
-			}
+						numbers.push(x);
+						console.log(numbers);
+								}
 					else{
 					console.log("This number is not even: " + x);
 				}
 			}
+				var div = document.getElementById('numList');
+				for(var i in numbers){
+					var p = document.createElement('p');
+					var content = document.createTextNode(numbers[i]);
+					p.appendChild(content);
+					div.appendChild(p);
+					}
 		}
 	}
-}
-generateNumbers();
-
-function runForm() {
-	generateNumbers();
-
-var startNum = document.forms["numberFun"]["startNum"].value;
-var endNum = document.forms["numberFun"]["endNum"].value;
-var stepNum = document.forms["numberFun"]["stepNum"].value;
-
-var displayButton = document.getElementById("displayBtn");
-
-displayButton.addEventListener("click" function()) {
-	function generateNumbers();
-});
-
-document.getElementById("printText").innerText = "Here are the even numbers between " + startNum + " and " + endNum + " by " + stepNum + "'s:"
-
 }
